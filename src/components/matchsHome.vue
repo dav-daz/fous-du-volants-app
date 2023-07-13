6b2fcaef9375c6a454fa866718774c97e2c7ce6c
David Duriez <duriez.dav@gmail.com>
2023-07-13 19:44:47 +0200

    Init calcul nbr terrains

--- src/components/matchsHome.vue
+++ src/components/matchsHome.vue
@@ -1,7 +1,7 @@
 <template>
   <button class="suffle" 
           type="button"
-          @click="suffle()">
+          @click="terrains(this.playersSelected.length)">
     Mélanger
   </button>
   <ul>
@@ -27,6 +27,49 @@ export default {
       this.sufflePlayersSelected = this.playersSelected.sort(() => Math.random() - 0.5);
 
       console.log(this.sufflePlayersSelected);
+    },
+    terrains(nbrPlayers){
+      var nbJoueurs = nbrPlayers;
+      var calcNbJoeursRestants = nbJoueurs%4;
+      var calcNbTerrains = (nbJoueurs - calcNbJoeursRestants) /4;
+
+      var calcNbTerrainsx4 = 0;
+      var calcNbTerrainsx2 = 0;
+      var calcNbTerrainsx3 = 0;
+      var calcNbTerrainsx5 = 0;
+
+
+      if(calcNbJoeursRestants === 2 || calcNbJoeursRestants === 3){
+        calcNbTerrains = calcNbTerrains + 1;
+      }
+
+      if(calcNbJoeursRestants === 0){
+        calcNbTerrainsx4 = calcNbTerrains;
+      }
+
+      if(calcNbJoeursRestants === 2){
+        calcNbTerrainsx2 = 1;
+        calcNbTerrainsx4 = (nbJoueurs - calcNbJoeursRestants) / 4;
+      }
+
+      if(calcNbJoeursRestants === 3){
+        calcNbTerrainsx3 = 1;
+        calcNbTerrainsx4 = (nbJoueurs - calcNbJoeursRestants) / 4;
+      }
+
+      if(calcNbJoeursRestants === 1){
+        calcNbTerrainsx5 = 1;
+        calcNbTerrainsx4 = (nbJoueurs - 5) / 4;
+      }
+
+      console.log('Nb joueurs : ' + nbJoueurs);
+      console.log('Nb terrains : ' + calcNbTerrains);
+      console.log('Nb joueurs restant : ' + calcNbJoeursRestants);
+      console.log('Nb terrains x4 : ' + calcNbTerrainsx4);
+      console.log('Nb terrains x3 : ' + calcNbTerrainsx3);
+      console.log('Nb terrains x5 : ' + calcNbTerrainsx5);
+      console.log('Nb terrains x2 : ' + calcNbTerrainsx2);
+      console.log('-----------');
     }
   }
 }
