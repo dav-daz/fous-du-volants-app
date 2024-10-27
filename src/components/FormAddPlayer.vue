f510caa0fdb9bd4e7c5a4bb0dfd892d2d2c5d20c
David Duriez <duriez.dav@gmail.com>
2024-10-27 02:43:55 +0200

    Ajout form ajout joueur + init store admin gestion joueurs

---
+++ src/components/FormAddPlayer.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from "vue";
+
+
+
+let prenom = ref("");
+</script>
+
+<template>
+<h2>Ajouter un joueur</h2>
+<form class="form-add-player">
+  <div class="form-content">
+    <div class="form-item">
+      <label for="email">Prénom: </label>
+      <input type="text" id="prenom" v-model="prenom">
+    </div>
+
+    <div class="form-actions">
+      <button class="modal-btn" type="button" @click="">Ajouter</button>
+    </div>
+  </div>
+</form>
+</template>
+
+<style lang="scss" scoped>
+.form-add-player {
+  label {
+    display: block;
+  }
+
+  .form-content {
+    display: flex;
+    align-items: flex-end;
+    gap: 2rem;
+    justify-content: space-between;
+  }
+
+  .form-actions {
+    text-align: right;
+    margin-top: 3rem;
+  }
+}
+</style>
